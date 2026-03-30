415c7bbbd368fe981626845b139d333ae4d1a98b
dheim123 <dheim@midland.k12.ia.us>
2026-03-30 11:57:13 -0500

    Add CreateTeam method to Classroom class
    Implement CreateTeam method to return a HashSet of team members.

--- Classroom.cs
+++ Classroom.cs
@@ -1,8 +1,8 @@
-﻿namespace knightmoves;
+namespace knightmoves;
 
-public class Classroom{
+public class Classroom {
 
-    public HashSet<string> CreateTeam(string first, string second, string third){
-        return null;
+    public HashSet<string> CreateTeam(string first, string second, string third) {
+        return new HashSet<string> { first, second, third };
     }
 }
